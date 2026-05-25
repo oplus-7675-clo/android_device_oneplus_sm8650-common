160047c36f3d9192fd865f43576757d62ff364fe
anky894 <anky894@proton.me>
2026-05-25 14:28:59 +0530

    Revert "sm8650-common: Shim WFD with libinput_shim"
    This reverts commit 8652bd35cd7ec8cd74d086aae0c31cbe462526a4.

--- extract-files.py
+++ extract-files.py
@@ -83,8 +83,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/my_product', '/product'),
     'system_ext/bin/horae': blob_fixup()
         .replace_needed('libprotobuf-cpp-lite.so', 'libprotobuf-cpp-lite-21.7.so'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libinput_shim.so'),
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'vendor/bin/init.kernel.post_boot-memory.sh': blob_fixup()

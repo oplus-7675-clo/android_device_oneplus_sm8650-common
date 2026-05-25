d383533d88108bd11681047450fd1f724989ed9d
anky894 <anky894@proton.me>
2026-05-25 14:31:02 +0530

    Revert "sm8650-common: Use a vendor copy of libtensorflow_lite_c"
    This reverts commit f3f69e604f88a02ba55d38f10c04193d1ee010de.

--- extract-files.py
+++ extract-files.py
@@ -105,12 +105,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('sensors.oplus.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
-    (
-        'vendor/lib64/libVoiceSdk.so',
-        'vendor/lib64/libcapiv2uvvendor.so',
-        'vendor/lib64/liblistensoundmodel2vendor.so',
-    ): blob_fixup()
-        .replace_needed('libtensorflowlite_c.so', 'libtensorflowlite_c_vendor.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
 }  # fmt: skip
